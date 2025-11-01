69416559950375119c41be8e66f28f112d8b73bf
hairizuanbinnoorazman <hairizuanbinnoorazman@gmail.com>
2025-11-01 23:29:30 +0800

    feat: A script to debug the jwt token being issued

---
+++ zzz.py
@@ -0,0 +1,17 @@
+import jwt
+from jwt import PyJWKClient
+import requests
+
+# Important - issuer is very strict
+# Every single character needs to be there
+DEX_URL = "http://localhost:9000/application/o/zzz/"
+
+jwks_url = f"{DEX_URL}.well-known/openid-configuration"
+jwks_uri = requests.get(jwks_url).json()["jwks_uri"]
+
+token = "XXX"
+jwks_client = PyJWKClient(jwks_uri)
+signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+decoded = jwt.decode(token, signing_key.key, algorithms=["RS256"], audience="XXX", issuer=DEX_URL)
+print(decoded)
