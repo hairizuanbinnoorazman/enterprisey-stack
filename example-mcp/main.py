08aeb1f81ecdd51da5b7dcdf7e0023265e071a6b
hairizuanbinnoorazman <hairizuanbinnoorazman@gmail.com>
2025-11-09 13:54:12 +0800

    feat: Added additional logs for handling authorization values

--- example-mcp/main.py
+++ example-mcp/main.py
@@ -9,13 +9,17 @@ Run:
 """
 
 from __future__ import annotations
+from typing import Any, Dict
 import os
 import re
-import logging
+import requests
 from pathlib import Path
 from typing import Optional
+import jwt
+from jwt import PyJWKClient
 
 from fastmcp import FastMCP, Context
+from fastmcp.utilities import logging
 
 BASE_DIR = Path(__file__).parent
 NOTES_DIR = BASE_DIR / "notes"
@@ -25,6 +29,35 @@ NOTES_DIR = BASE_DIR / "notes"
 # -----------------
 mcp = FastMCP("Demo MCP Server")
 
+# Setting environment variables
+OIDC_CLIENT_ID: str = os.environ["OIDC_CLIENT_ID"]
+OIDC_CLIENT_SECRET: str = os.environ["OIDC_CLIENT_SECRET"]
+OIDC_ISSUER: str = os.environ["OIDC_ISSUER"]
+
+logger = logging.get_logger('MAIN')
+logger.info(OIDC_CLIENT_ID)
+logger.info(OIDC_CLIENT_SECRET)
+logger.info(OIDC_ISSUER)
+JWKS_URI = requests.get(f"{OIDC_ISSUER}/.well-known/openid-configuration").json()["jwks_uri"]
+
+async def retrieve_user(context: Context) -> None:
+    if context.request_context.meta is not None:
+        ll = context.request_context.meta.model_dump()
+        await context.info(f"Context Meta 0 :: {context.request_context.meta is not None}")
+        await context.info(f"Context Meta 1 :: {ll}")
+        await context.info(f"Context Meta 2 :: {ll.keys()}")
+        await context.info(f"Context Meta 3 :: {ll['headers']}")
+        await context.info(f"Context Meta 4 :: {ll['headers'].keys()}")
+        await context.info(f"Context Meta 5 :: {ll['headers']['authorization']}")
+        authorization_val: str = ll['headers']['authorization']
+        authorization_val = authorization_val.split(' ')[1]
+        jwks_client = PyJWKClient(JWKS_URI)
+        signing_key = jwks_client.get_signing_key_from_jwt(authorization_val)
+        decoded = jwt.decode(authorization_val, signing_key.key, algorithms=["RS256"], audience=OIDC_CLIENT_ID, issuer=OIDC_ISSUER)
+        await context.info(f"Context Meta 6 :: {decoded}")
+        await context.info(f"Context Meta 7 :: {decoded['email']}")
+
+
 # -----------------
 # Tools
 # -----------------
@@ -32,6 +65,7 @@ mcp = FastMCP("Demo MCP Server")
 async def add(context: Context, a: int, b: int) -> int:
     """Add two integers and return the sum."""
     await context.info(f"DAAAAMMMMMNNN: {context.request_context.meta}")
+    await retrieve_user(context)
     await context.info(f"Client ID: {context.client_id}")
     await context.info(f"Session ID: {context.session_id}")
     await context.info(f"{context.get_http_request().headers}")
